31783404aeabce80cdcd24c384b76a356acbe0a0
hcz017 <hcz017@gmail.com>
2017-07-12 21:36:46 +0800

    initial commit

---
+++ app/src/main/java/com/hcz017/drawingview/DrawingView.java
@@ -0,0 +1,293 @@
+package com.hcz017.drawingview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.support.annotation.ColorInt;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class DrawingView extends View {
+    private static final String TAG = "DrawingView";
+    private static final float TOUCH_TOLERANCE = 4;
+    private Bitmap mBitmap;
+    private Bitmap mOriginBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint mBitmapPaint;
+    private Paint mPaint;
+    private boolean mDrawMode;
+    private float mX, mY;
+    private float mProportion = 0;
+    private LinkedList<DrawPath> savePath;
+
+    public DrawingView(Context c) {
+        this(c, null);
+    }
+
+    public DrawingView(Context c, AttributeSet attrs) {
+        this(c, attrs, 0);
+    }
+
+    public DrawingView(Context c, AttributeSet attrs, int defStyle) {
+        super(c, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        Log.d(TAG, "init: ");
+        mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+        mDrawMode = false;
+        savePath = new LinkedList<>();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (mBitmap != null) {
+            // 根据加载的图片尺寸缩放控件,这里只考虑了高大于宽的情况
+            if (mBitmap.getHeight() > heightSize) {
+                widthSize = heightSize * mBitmap.getWidth() / mBitmap.getHeight();
+            } else {
+                heightSize = mBitmap.getHeight();
+                widthSize = mBitmap.getWidth();
+            }
+        }
+        Log.d(TAG, "onMeasure: heightSize: " + heightSize + " widthSize: " + widthSize);
+        setMeasuredDimension(widthSize, heightSize);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mBitmap == null) {
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        }
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(Color.TRANSPARENT);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 根据图片尺寸缩放图片，同样只考虑了高大于宽的情况
+        float proportion = (float) canvas.getHeight() / mBitmap.getHeight();
+        if (proportion < 1) {
+            Matrix matrix = new Matrix();
+            mProportion = proportion;
+            matrix.postScale(proportion, proportion);
+            matrix.postTranslate((canvas.getWidth() - mBitmap.getWidth() * proportion) / 2, 0);
+            canvas.drawBitmap(mBitmap, matrix, mBitmapPaint);
+        } else {
+            mProportion = 0;
+            canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // 如果你的界面有多个模式，你需要有个变量来判断当前是否可draw
+        if (!mDrawMode) {
+            return false;
+        }
+        float x;
+        float y;
+        if (mProportion != 0) {
+            x = (event.getX()) / mProportion;
+            y = event.getY() / mProportion;
+        } else {
+            x = event.getX();
+            y = event.getY();
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath = new Path();
+                mPath.reset();
+                mPath.moveTo(x, y);
+                mX = x;
+                mY = y;
+                mCanvas.drawPath(mPath, mPaint);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dx = Math.abs(x - mX);
+                float dy = Math.abs(y - mY);
+                if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                    mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+                    mX = x;
+                    mY = y;
+                }
+                mCanvas.drawPath(mPath, mPaint);
+                break;
+            case MotionEvent.ACTION_UP:
+                mPath.lineTo(mX, mY);
+                mCanvas.drawPath(mPath, mPaint);
+                savePath.add(new DrawPath(mPath, mPaint.getColor(), mPaint.getStrokeWidth()));
+                mPath = null;
+                break;
+            default:
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void initializePen() {
+        mDrawMode = true;
+        mPaint = null;
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setFilterBitmap(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        mCanvas.drawColor(color);
+        super.setBackgroundColor(color);
+    }
+
+    public void setPenSize(float size) {
+        mPaint.setStrokeWidth(size);
+    }
+
+    public float getPenSize() {
+        return mPaint.getStrokeWidth();
+    }
+
+    public void setPenColor(@ColorInt int color) {
+        mPaint.setColor(color);
+    }
+
+    public
+    @ColorInt
+    int getPenColor() {
+        return mPaint.getColor();
+    }
+
+    /**
+     * @return 当前画布上的内容
+     */
+    public Bitmap getImageBitmap() {
+        return mBitmap;
+    }
+
+    public void loadImage(Bitmap bitmap) {
+        Log.d(TAG, "loadImage: ");
+        mOriginBitmap = bitmap;
+        mBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        mCanvas = new Canvas(mBitmap);
+        invalidate();
+    }
+
+    public void undo() {
+        Log.d(TAG, "undo: recall last path");
+        if (savePath != null && savePath.size() > 0) {
+            // 清空画布
+            mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            loadImage(mOriginBitmap);
+
+            savePath.removeLast();
+
+            // 将路径保存列表中的路径重绘在画布上 遍历绘制
+            for (DrawPath dp : savePath) {
+                mPaint.setColor(dp.getPaintColor());
+                mPaint.setStrokeWidth(dp.getPaintWidth());
+                mCanvas.drawPath(dp.path, mPaint);
+            }
+            invalidate();
+        } else {
+            // TODO: 2017/7/12 这里留一个小bug
+            // 当第一笔和最后一笔画笔的样式（颜色和宽度）不同的时候，全部撤销后，这时画笔的样式被设置成了和
+            // 第一笔一样，此时再绘画使用的是和第一笔一样的样式，而工具栏上我们看到的是最后一笔的样式。
+        }
+    }
+
+    /**
+     * 保存图片，其实我个人建议在其他类里面写保存方法，{@link #getImageBitmap()}就是内容
+     * @param filePath 路径名
+     * @param filename 文件名
+     * @param format 存储格式
+     * @param quality 质量
+     * @return 是否保存成功
+     */
+    public boolean saveImage(String filePath, String filename, Bitmap.CompressFormat format,
+                             int quality) {
+        if (quality > 100) {
+            Log.d("saveImage", "quality cannot be greater that 100");
+            return false;
+        }
+        File file;
+        FileOutputStream out = null;
+        try {
+            switch (format) {
+                case PNG:
+                    file = new File(filePath, filename + ".png");
+                    out = new FileOutputStream(file);
+                    return mBitmap.compress(Bitmap.CompressFormat.PNG, quality, out);
+                case JPEG:
+                    file = new File(filePath, filename + ".jpg");
+                    out = new FileOutputStream(file);
+                    return mBitmap.compress(Bitmap.CompressFormat.JPEG, quality, out);
+                default:
+                    file = new File(filePath, filename + ".png");
+                    out = new FileOutputStream(file);
+                    return mBitmap.compress(Bitmap.CompressFormat.PNG, quality, out);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 路径对象
+     */
+    private class DrawPath {
+        Path path;
+        int paintColor;
+        float paintWidth;
+
+        DrawPath(Path path, int paintColor, float paintWidth) {
+            this.path = path;
+            this.paintColor = paintColor;
+            this.paintWidth = paintWidth;
+        }
+
+        int getPaintColor() {
+            return paintColor;
+        }
+
+        float getPaintWidth() {
+            return paintWidth;
+        }
+    }
+}
---
+++ app/src/main/java/com/hcz017/drawingview/MainActivity.java
@@ -0,0 +1,81 @@
+package com.hcz017.drawingview;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DrawingView mDrawingView;
+    private static int COLOR_PANEL = 0;
+    private static int BRUSH = 0;
+    private ImageButton mColorPanel;
+    private ImageButton mBrush;
+    private ImageButton mUndo;
+    private ImageButton mSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        initPaintMode();
+        loadImage();
+    }
+
+    private void initViews() {
+        mDrawingView = (DrawingView) findViewById(R.id.img_screenshot);
+        mBrush = (ImageButton) findViewById(R.id.brush);
+        mColorPanel = (ImageButton) findViewById(R.id.color_panel);
+        mUndo = (ImageButton) findViewById(R.id.undo);
+        mSave = (ImageButton) findViewById(R.id.save);
+
+        mBrush.setOnClickListener(this);
+        mColorPanel.setOnClickListener(this);
+        mUndo.setOnClickListener(this);
+        mSave.setOnClickListener(this);
+        initPaintMode();
+    }
+
+    private void initPaintMode() {
+        mDrawingView.initializePen();
+        mDrawingView.setPenSize(10);
+        mDrawingView.setPenColor(getColor(R.color.red));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.brush:
+                mBrush.setImageResource(BRUSH == 0 ? R.drawable.ic_brush : R.drawable.ic_pen);
+                mDrawingView.setPenSize(BRUSH == 0 ? 40 : 10);
+                BRUSH = 1 - BRUSH;
+                break;
+            case R.id.color_panel:
+                mColorPanel.setImageResource(COLOR_PANEL == 0 ? R.drawable.ic_color_blue : R.drawable.ic_color_red);
+                mDrawingView.setPenColor(COLOR_PANEL == 0 ? getColor(R.color.blue) : getColor(R.color.red));
+                COLOR_PANEL = 1 - COLOR_PANEL;
+                break;
+            case R.id.undo:
+                mDrawingView.undo();
+                break;
+            case R.id.save:
+                String sdcardPath = Environment.getExternalStorageDirectory().toString();
+                if (mDrawingView.saveImage(sdcardPath, "DrawImg", Bitmap.CompressFormat.PNG, 100)){
+                    Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void loadImage() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.raw.image);
+        mDrawingView.loadImage(bitmap);
+    }
+}
