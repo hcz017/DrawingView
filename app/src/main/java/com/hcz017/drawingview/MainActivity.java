17f29d9bd688b81e3f550b6d8de1891d066b32ba
hcz017 <hcz017@gmail.com>
2019-12-08 15:42:14 +0800

    Add permission check and request

--- app/src/main/java/com/hcz017/drawingview/MainActivity.java
+++ app/src/main/java/com/hcz017/drawingview/MainActivity.java
@@ -1,9 +1,13 @@
 package com.hcz017.drawingview;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton mBrush;
     private ImageButton mUndo;
     private ImageButton mSave;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initViews();
         initPaintMode();
         loadImage();
+        checkPermissions();
     }
 
     private void initViews() {
@@ -68,12 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mDrawingView.undo();
                 break;
             case R.id.save:
-                String sdPicturesPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
-                String filename = sdPicturesPath  + "/DrawImage_" + timeStamp;
-                if (FileUtils.saveBitmap(filename, mDrawingView.getImageBitmap(), Bitmap.CompressFormat.PNG, 100)) {
-                    Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show();
-                }
+                saveImage();
                 break;
             default:
                 break;
@@ -84,4 +85,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.raw.image);
         mDrawingView.loadImage(bitmap);
     }
+
+    public void saveImage() {
+        if (!checkPermissions()) {
+            return;
+        }
+        String sdPicturesPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String filename = sdPicturesPath + "/DrawImage_" + timeStamp;
+        if (FileUtils.saveBitmap(filename, mDrawingView.getImageBitmap(), Bitmap.CompressFormat.PNG, 100)) {
+            Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return false;
+        } else {
+            // Permission has already been granted
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 }
